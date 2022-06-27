cb08d5b45a071e61fde8a0b274e8c3c04b722c1c
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-06-27 21:48:16 +0200

    css and js adjustmets for proper slider functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const container = document.getElementById("container");
 const clearButton = document.getElementById("clearbutton");
 const griditem = document.getElementsByClassName("grid-item");
 const erasebutton = document.getElementById("eraserbutton");
+const slider= document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 let erasestate = false;
 
 //functions
@@ -22,28 +25,35 @@ function makeRows(rows, cols){
 //TODO: add whatever color the user chooses, 
 function colorGrid(e) {
     if(erasestate == false) {
-        e.target.style.backgroundColor = 'black' 
+        e.target.style.backgroundColor = 'black' ;
     } else if(erasestate == true){ 
-        e.target.style.backgroundColor = 'white' 
+        e.target.style.backgroundColor = 'white' ;
     }
 }
 
 function addListener() {
     for(i = 0; i < container.childElementCount;i ++){
-        griditem[i].addEventListener('mousedown', colorGrid)
+        griditem[i].addEventListener('mousedown', colorGrid);
     }
 }
 
 function clear() {
     container.innerHTML = '';
-    makeRows(6, 6);
+    makeRows(slider.value, slider.value);
     addListener();
 }
 
 //HERE THE WEBSITE STARTS WORKING
-makeRows(6, 6);
+makeRows(slider.value, slider.value);
 addListener();
-clearButton.addEventListener('click', clear)
-erasebutton.onclick = function() { erasestate = !erasestate; }
+clearButton.addEventListener('click', clear);
+erasebutton.onclick = function() { erasestate = !erasestate; };
+slider.oninput = function () {
+    output.innerHTML = this.value ;
+}
+slider.onchange = function () {
+    clear()
+}
+
 
 
