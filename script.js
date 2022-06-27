44b39ca4091f25815993c57740aeca4003414a5a
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-06-27 17:43:34 +0200

    basic grid functionality, clear board function added, bit of work on the menu

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 //declarations of constants and variables
 
 const container = document.getElementById("container");
-
+const clearButton = document.getElementById("clearbutton");
+const griditem = document.getElementsByClassName("grid-item")
 
 //functions
+//TODO: dynamic size allocation, 
 function makeRows(rows, cols){
     container.style.setProperty('--grid-rows',rows);
     container.style.setProperty('--grid-cols',cols);
     for(c=0; c<rows*cols; c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c+1)
-        container.appendChild(cell).className = "grid-item"
+        //cell.innerText = (c+1);
+        container.appendChild(cell).className = "grid-item";
+        container.appendChild(cell).addEventListener('mousedown',colorGrid);
     };
+    
 };
 
+//TODO: add whatever color the user chooses, 
+function colorGrid(e, color='black') {
+    e.target.style.backgroundColor = color;
+}
 
+
+
+function clear() {
+    container.innerHTML = '';
+    makeRows(4, 4);
+}
 
 //HERE THE WEBSITE STARTS WORKING
 makeRows(4, 4);
+clearButton.addEventListener('click', colorGrid(e,'black'))
